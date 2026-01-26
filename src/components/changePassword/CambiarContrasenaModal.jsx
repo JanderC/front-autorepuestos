aac414a07af66840a8c8e30417e343ff7c598437
JanderC <janderalexisc@gmail.com>
2026-01-26 11:13:48 -0400

    cambios

--- src/components/changePassword/CambiarContrasenaModal.jsx
+++ src/components/changePassword/CambiarContrasenaModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Lock, Eye, EyeOff, AlertCircle, CheckCircle } from 'lucide-react';
-import * as api from '../../services/api';
+import api from '../../services/api';
 
 const CambiarContrasenaModal = ({ isOpen, onClose, onSuccess }) => {
   const [usuarios, setUsuarios] = useState([]);
